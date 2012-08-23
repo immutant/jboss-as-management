ebd4d99f594b6d4de0c66db2718c9b1083ca577b
Tobias Crawley <tcrawley@gmail.com>
2012-08-23 17:51:49 -0400

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jboss-as-management "0.1.0"
+(defproject jboss-as-management "0.1.1"
   :description "A thin wrapper around the JBoss AS7 management API."
   :url "https://github.com/immutant/jboss-as-management"
   :license {:name "Eclipse Public License"
