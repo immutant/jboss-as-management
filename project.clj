e7438c1bf3a288e65967efc6d737409052180882
Toby Crawley <toby@tcrawley.org>
2013-01-28 09:59:02 -0500

    Bump data.json dep to 0.2.1.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :exclusions [org.clojure/clojure]
-  :dependencies [[org.clojure/data.json "0.2.0"]
+  :dependencies [[org.clojure/data.json "0.2.1"]
                  [clj-http-lite         "0.2.0"]]
   :lein-release {:deploy-via :clojars})
