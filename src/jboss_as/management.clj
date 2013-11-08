f7b94894c24c381f06b88beac2701f99b2841fb7
Jim Crossley <jim@crossleys.org>
2013-11-08 18:57:27 -0500

    Using method maps now, and rolled :add into :deploy

--- src/jboss_as/management.clj
+++ src/jboss_as/management.clj
@@ -1,17 +1,16 @@
 (ns jboss-as.management
   (:require [clojure.java.shell   :as shell]
-            [clojure.data.json    :as json]
-            [clj-http.lite.client :as client]
             [jboss-as.command     :as cmd])
   (:use [jboss-as.api :only (request host-controller server-group port)]))
 
 (defprotocol Server
-  "Things you can do to a JBoss server"
+  "Things you can do to a JBoss server. Names should always have a
+   valid suffix, e.g. clj, ima, war"
   (start [_] "Start the server")
   (stop [_] "Stop the server")
   (ready? [_] "Is the server up?")
-  (add [_ name content-uri] "Identify deployment content")
-  (deploy [_ name] "Deploy the added content")
+  (deploy [_ name uri] [_ uri] "Deploy the content at uri")
+  (deployed? [_ name] "Currently deployed?")
   (undeploy [_ name] "Undeploy by removing associated content"))
 
 (deftype Standalone [uri, command])
@@ -26,74 +25,92 @@
       (->Domain uri (cmd/domain opts))
       (->Standalone uri (cmd/standalone opts)))))
 
-(extend-type Standalone
-  Server
-  (start [this]
-    (if (ready? this)
-      (throw (Exception. "JBoss is already running!")))
-    (println (.command this))
-    (future (apply shell/sh (.split (.command this) " +"))))
-  (stop [this]
-    (request (.uri this), :operation "shutdown"))
-  (ready? [this]
-    (try (let [response (request (.uri this), :operation "read-attribute" :name "server-state")]
-           (and response
-                (= "success" (response :outcome))
-                (= "running" (response :result))))
-         (catch Exception _)))
-  (add [this name content-uri]
-    (request (.uri this),
-             :operation "add"
-             :address ["deployment" name]
-             :content [{:url (.toExternalForm content-uri)}]))
-  (deploy [this name]
-    (let [result (request (.uri this), :operation "deploy" :address ["deployment" name])]
-      (if (= "success" (:outcome result))
-        result
-        (throw (Exception. (-> (:failure-description result) first val first val))))))
-  (undeploy [this name]
-    (request (.uri this), :operation "remove" :address ["deployment" name])))
+(def common
+  {:start     (fn [this]
+                (if (ready? this)
+                  (throw (Exception. "JBoss is already running!")))
+                (println (.command this))
+                (future (apply shell/sh (.split (.command this) " +"))))
+   :deployed? (fn [this name]
+                (->> (request (.uri this)
+                              :operation "read-children-names"
+                              :child-type "deployment")
+                     :result
+                     (some #{name})
+                     boolean))})
 
-(extend-type Domain
+(def standalone
+  {:stop     (fn [this]
+               (request (.uri this), :operation "shutdown"))
+   :ready?   (fn [this]
+               (try (let [response (request (.uri this)
+                                            :operation "read-attribute"
+                                            :name "server-state")]
+                      (and response
+                           (= "success" (response :outcome))
+                           (= "running" (response :result))))
+                    (catch Exception _)))
+   :deploy   (fn [this name content-uri]
+               (if (deployed? this name) (undeploy this name))
+               (request (.uri this),
+                        :operation "add"
+                        :address ["deployment" name]
+                        :content [{:url (.toExternalForm content-uri)}])
+               (let [result (request (.uri this),
+                                     :operation "deploy"
+                                     :address ["deployment" name])]
+                 (if (= "success" (:outcome result))
+                   result
+                   (throw (Exception. (str result))))))
+   :undeploy (fn [this name]
+               (request (.uri this),
+                        :operation "remove"
+                        :address ["deployment" name]))})
+
+(def domain
+  {:stop     (fn [this]
+               (request (.uri this),
+                        :operation "shutdown"
+                        :address [:host (first (host-controller (.uri this)))]))
+   :ready?   (fn [this]
+               (try
+                 (> (-> (host-controller (.uri this)) last :server count) 1)
+                 (catch Exception _)))
+   :deploy   (fn [this name content-uri]
+               (if (deployed? this name) (undeploy this name))
+               (let [uri (.uri this)
+                     group (server-group uri)]
+                 (request (.uri this),
+                          :operation "add"
+                          :address ["deployment" name]
+                          :content [{:url (.toExternalForm content-uri)}])
+                 (request (.uri this),
+                          :operation "add"
+                          :address [{:server-group group} {:deployment name}]
+                          :content [{:url (.toExternalForm content-uri)}])
+                 (let [result (request uri,
+                                       :operation "deploy"
+                                       :address [{:server-group group} {:deployment name}])]
+                   (if (= "success" (:outcome result))
+                     result
+                     (throw (Exception. (str result)))))))
+   :undeploy (fn [this name]
+               (let [uri (.uri this)
+                     group (server-group uri)]
+                 (request uri,
+                          :operation "remove"
+                          :address [{:server-group group} {:deployment name}])
+                 (request uri,
+                          :operation "remove"
+                          :address ["deployment" name])))})
+
+(extend Standalone
   Server
-  (start [this]
-    (if (ready? this)
-      (throw (Exception. "JBoss is already running!")))
-    (println (.command this))
-    (future (apply shell/sh (.split (.command this) " +"))))
-  (stop [this]
-    (request (.uri this),
-             :operation "shutdown"
-             :address [:host (first (host-controller (.uri this)))]))
-  (ready? [this]
-    (try
-      (> (-> (host-controller (.uri this)) last :server count) 1)
-      (catch Exception _)))
-  (add [this name content-uri]
-    (let [uri (.uri this)
-          group (server-group uri)]
-      (request (.uri this),
-               :operation "add"
-               :address ["deployment" name]
-               :content [{:url (.toExternalForm content-uri)}])
-      (request (.uri this),
-               :operation "add"
-               :address [{:server-group group} {:deployment name}]
-               :content [{:url (.toExternalForm content-uri)}])))
-  (deploy [this name]
-    (let [uri (.uri this)
-          group (server-group uri)
-          result (request uri,
-                          :operation "deploy"
-                          :address [{:server-group group} {:deployment name}])]
-      (if (= "success" (:outcome result))
-        result
-        (throw (Exception. (str result))))))
-  (undeploy [this name]
-    (let [uri (.uri this)
-          group (server-group uri)]
-      (request uri, :operation "remove" :address [{:server-group group} {:deployment name}])
-      (request uri, :operation "remove" :address ["deployment" name]))))
+  (merge common standalone))
+
+(extend Domain
+  Server
+  (merge common domain))
 
 (defn wait-for-ready?
   "Returns true if the server is up. Otherwise, sleeps for one second
