eae812482a40bdd80b79e3ef9fb1f36dba54c0c1
Toby Crawley <toby@tcrawley.org>
2013-11-11 11:20:05 -0500

    Move the clojure dep to the :dev profile.

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "https://github.com/immutant/jboss-as-management"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.1"]
+  :exclusions [org.clojure/clojure]
+  :dependencies [[org.clojure/data.json "0.2.1"]
                  [clj-http-lite         "0.2.0"]]
   :signing {:gpg-key "BFC757F9"}
-  :lein-release {:deploy-via :clojars})
+  :lein-release {:deploy-via :clojars}
+  :profiles {:dev
+             {:dependencies [[org.clojure/clojure "1.5.1"]]}})
