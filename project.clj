9edfdd7d4c05a05717432a56c49766aae649e105
Toby Crawley <toby@tcrawley.org>
2014-08-12 15:54:54 -0400

    Move to 0.4.0 development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jboss-as-management "0.3.3-SNAPSHOT"
+(defproject jboss-as-management "0.4.0-SNAPSHOT"
   :description "A thin wrapper around the JBoss AS7 management API."
   :url "https://github.com/immutant/jboss-as-management"
   :license {:name "Eclipse Public License"
@@ -9,4 +9,4 @@
   :signing {:gpg-key "BFC757F9"}
   :lein-release {:deploy-via :clojars}
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+             {:dependencies [[org.clojure/clojure "1.6.0"]]}})
