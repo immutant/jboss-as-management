0cbb4bedbfeaa3d5fa25241ba02e280dc9238567
Toby Crawley <toby@tcrawley.org>
2015-11-12 11:01:25 -0500

    Revert "Add debug output."
    This reverts commit 8a112eb6ee3444c69f81ccc08e5e4be5034b5a10.

--- src/jboss_as/api.clj
+++ src/jboss_as/api.clj
@@ -5,11 +5,6 @@
 ;;; We assume the default management-http port
 (def port 9990)
 
-(defn mark [& msg]
-  (let [ts (.format (java.text.SimpleDateFormat. "HH:mm:ss,SSS")
-                 (java.util.Date.))]
-    (apply println ts msg)))
-
 (defn request
   "Params assembled into a hash that is passed to the JBoss management
    interface via the passed URI and returns the JBoss response as a
@@ -19,12 +14,8 @@
         response (client/post uri {:body body
                                    :headers {"Content-Type" "application/json"}
                                    :throw-exceptions false})]
-    (mark "TC: api request" uri (apply hash-map params))
     (if-let [body (:body response)]
-      (let [data (json/read-str body :key-fn keyword)]
-        (mark "TC: api data" data)
-        data)
-      (mark "TC: api call returned nil"))))
+      (json/read-str body :key-fn keyword))))
 
 (defn request-or-toss
   "Throws an Exception if the request isn't successful"
